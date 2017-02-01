2c55a0fcb6d6328ed3c0fa81f21dc5f341cd1ae0
Shravan Kumar <kum_shravan@yahoo.com>
2017-02-01 17:14:21 +0530

    Added IK to the player and NPCs.

--- Source/HuntersTrials/HuntersTrialsHUD.cpp
+++ Source/HuntersTrials/HuntersTrialsHUD.cpp
@@ -9,7 +9,7 @@
 AHuntersTrialsHUD::AHuntersTrialsHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshiarTexObj(TEXT("/Game/Player/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshiarTexObj(TEXT("/Game/Static/Player/Textures/FirstPersonCrosshair"));
 	CrosshairTex = CrosshiarTexObj.Object;
 }
 

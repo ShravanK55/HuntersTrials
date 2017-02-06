f405634bddf28c1a6dc92ba429e60a6e7fe37321
Shravan Kumar <kum_shravan@yahoo.com>
2017-02-06 19:58:04 +0530

    Refactored to have a single character, moved the level into the dynamic folder.

--- Source/HuntersTrials/HuntersTrialsGameMode.cpp
+++ Source/HuntersTrials/HuntersTrialsGameMode.cpp
@@ -3,13 +3,12 @@
 #include "HuntersTrials.h"
 #include "HuntersTrialsGameMode.h"
 #include "HuntersTrialsHUD.h"
-#include "Player/FirstPersonCharacter.h"
 
 AHuntersTrialsGameMode::AHuntersTrialsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behaviour/PlayerCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Character/Behaviour/GameCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
